5a3ed092021d9db3ea582cf618999d0cfbcc566f
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-11-20 23:23:48 +0100

    Fixed data-update bug

--- data-update.sh
+++ data-update.sh
@@ -1,2 +1,2 @@
 curl -o owid-covid-data.csv https://covid.ourworldindata.org/data/owid-covid-data.csv
-curl -o covid-variants.csv https://covid.ourworldindata.org/data/covid-variants.csv
+curl -o covid-variants.csv https://covid.ourworldindata.org/data/variants/covid-variants.csv
