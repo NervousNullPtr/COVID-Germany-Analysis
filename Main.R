0621fd02532f4c19428cabe4364a15b77b06b440
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-11-25 18:45:31 +0100

    Better lib-loading, new plot

--- Main.R
+++ Main.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+library(readr)
 library(ggplot2)
 library(scales)
 Sys.setlocale("LC_ALL", "en_US.utf8")
@@ -7,7 +7,7 @@ plot_time_format        <- "%Y-%m-%d"
 plot_loc                <- "Germany"
 plot_variant            <- "Delta"
 plot_cd_thres           <- 0
-plot_prim_y_breaks      <- seq(0, 70000, by = 5000)
+plot_prim_y_breaks      <- seq(0, 100000, by = 5000)
 plot_sec_y_breaks       <- seq(0, 100, by = 5)
 plot_colors             <- c("firebrick2",
                              "#00095c",
